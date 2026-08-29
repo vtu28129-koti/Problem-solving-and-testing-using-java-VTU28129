71e8001d7861a1850ab0042f930be485616f40f4
vtu28129-koti <vtu28129@veltech.edu.in>
2026-08-29 10:22:01 +0530

    Add moveZeroes method to Solution class
    Implement moveZeroes method to shift non-zero elements.

---
+++ WEEK4 VTU28129/TASK6.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
+
+OUTPUT:
+Input
+nums =
+[0,1,0,3,12]
+Output
+[1,3,12,0,0]
