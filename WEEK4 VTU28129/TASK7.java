97f18fbdc99b8ec4e837a29092799534f6b6a219
vtu28129-koti <vtu28129@veltech.edu.in>
2026-09-03 09:21:20 +0530

    Create TASK7.java

---
+++ WEEK4 VTU28129/TASK7.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int n = arr.size();
+
+        int primary = 0;
+        int secondary = 0;
+
+        for (int i = 0; i < n; i++) {
+            primary += arr.get(i).get(i);
+            secondary += arr.get(i).get(n - 1 - i);
+        }
+
+        return Math.abs(primary - secondary);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j < n; j++) {
+                row.add(sc.nextInt());
+            }
+
+            arr.add(row);
+        }
+
+        System.out.println(diagonalDifference(arr));
+
+        sc.close();
+    }
+}
+
+OUTPUT:
+Input (stdin)
+3
+11 2 4
+4 5 6
+10 8 -12
+Your Output (stdout)
+15
