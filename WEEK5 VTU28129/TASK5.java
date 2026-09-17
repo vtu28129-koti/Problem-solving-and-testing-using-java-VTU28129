d62e1f0b3d8f9af70d3b0f8ba8148d89cb8d4691
vtu28129-koti <vtu28129@veltech.edu.in>
2026-09-17 09:20:12 +0530

    Create TASK5.java

---
+++ WEEK5 VTU28129/TASK5.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int myAtoi(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        int i = 0;
+        int n = s.length();
+        
+        // 1. Skip leading whitespaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+        
+        if (i == n) {
+            return 0;
+        }
+        
+        // 2. Check optional sign
+        int sign = 1;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+        
+        // 3. Convert characters to integer & handle 32-bit overflow
+        int result = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+            
+            // Check for potential overflow before updating result
+            if (result > Integer.MAX_VALUE / 10 || 
+               (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            
+            result = result * 10 + digit;
+            i++;
+        }
+        
+        return result * sign;
+    }
+}
+
+OUTPUT:
+Input
+s =
+"42"
+Output
+42
