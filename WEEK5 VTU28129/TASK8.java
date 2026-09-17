ea5f8ef2f6954af8360dfcefb44a976c30d0857b
vtu28129-koti <vtu28129@veltech.edu.in>
2026-09-17 09:43:10 +0530

    Add method to find words matching a pattern
    Implement a solution to find and replace words matching a given pattern.

---
+++ WEEK5 VTU28129/TASK8.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> result = new ArrayList<>();
+        for (String word : words) {
+            if (matches(word, pattern)) {
+                result.add(word);
+            }
+        }
+        return result;
+    }
+
+    private boolean matches(String word, String pattern) {
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+
+        for (int i = 0; i < word.length(); i++) {
+            char w = word.charAt(i);
+            char p = pattern.charAt(i);
+
+            if (m1[w] != m2[p]) {
+                return false;
+            }
+
+            // Store position + 1 to avoid default 0 conflict
+            m1[w] = i + 1;
+            m2[p] = i + 1;
+        }
+
+        return true;
+    }
+}
+
+OUTPUT:
+Input
+words =
+["abc","deq","mee","aqq","dkd","ccc"]
+pattern =
+"abb"
+Output
+["mee","aqq"]
+Expected
+["mee","aqq"]
